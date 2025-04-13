fc98d884774f549796de856b2f842eedac304864
Publ1sher <akilshalabayev@gmail.com>
2025-04-13 21:10:13 +0500

    feat: add RecyclerView with country data (name,population, flag)

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.lab6
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val selectedTextView: TextView = findViewById(R.id.selectedCountryTextView)
+
+        val countries = listOf(
+            Country("Vietnam", "98,000,000", R.drawable.images),
+            Country("United States", "320,000,000", R.drawable.usa),
+            Country("Russia", "142,000,000", R.drawable.russia)
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CountryAdapter(countries) { selectedCountry ->
+            selectedTextView.text = "Selected: ${selectedCountry.name} (Population: ${selectedCountry.population})"
+            selectedTextView.visibility = TextView.VISIBLE
         }
     }
 }
